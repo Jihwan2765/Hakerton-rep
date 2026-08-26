cc6c2df1e56c4a0d4bf2f02c78b6a66d34e0e519
jihwan park <chong00091@gmail.com>
2026-08-27 05:34:15 +0900

    중간 저장

---
+++ Source/HackertonTest/TileSortLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "TileSortLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HACKERTONTEST_API UTileSortLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
